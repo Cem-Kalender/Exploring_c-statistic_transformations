e8bff41b471577072ace8df8f1da03763c6dd67f
Cem-Kalender <92435506+Cem-Kalender@users.noreply.github.com>
2022-05-19 14:21:43 +0200

    Update simulation_study.R

--- simulation_study.R
+++ simulation_study.R
@@ -7,38 +7,16 @@ library(mice)
 library(tidyverse)
 source('FUNCTIONS.R')
 
-n.imp = 30 # between-variance
-nr_boots = 50 #within-variance
-# simulate study
+n.imp = 5 # between-variance
 
-study.results = simulation.study(repetitions = 2, 
-                                 n.imp = n.imp,
-                                 nr_boots = nr_boots, 
-                                 mech = 'MAR', 
-                                 var = 'covariates')
+# exectute simulation study
+res = simulation.study.Altman(repetitions = 10000, 
+                       n.imp = 30, 
+                       mech = 'MAR', 
+                       var = 'covariates')
 
-
-study.results
-capture.output(study.results, file = " ")
-# calculate convergence rate
-convergence.rate(study.results)
-
+# display convergence
+convergence.rate(res)
+res
 # visualize results
-# Opening the graphical device
-pdf("plots_MAR_covariates_1000.pdf")
-# Creating a plot
-plots = forestplot(study.results)
-# Closing the graphical device
-dev.off() 
-
-## MNAR on outcome
-pattern_list = list(c(1,1,0))
-prob = .5
-mech_list = 'MNAR'
-
-
-study.results.2 = system.time(simulation.study.loop(repetitions = 5, 
-                                                    n.imp = n.imp,
-                                                    nr_boots = nr_boots, 
-                                                    mech = 'MAR', 
-                                                    var = 'outcome'))
+forestplot(res)
